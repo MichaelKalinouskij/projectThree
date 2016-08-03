6f288ab4c68517b5a82ee0988596b70091fa861e
MichaelKalinouskij <1135323@gmail.com>
2016-08-03 13:53:49 +0300

    projectthree

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        watch: {
+            sass: {
+                files: 'sass/style.scss',
+                tasks: ['sass']
+            }
+        },
+        sass: {                              // Task
+            dist: {                            // Target
+                options: {                       // Target options
+                    style: 'expanded',
+                    sourcemap: 'none'
+                },
+                files: {                         // Dictionary of files
+                    'style/style.css': 'sass/style.scss'       // 'destination': 'source
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.registerTask('default', ['sass']);
+    grunt.loadNpmTasks('grunt-contrib-watch')
+};
